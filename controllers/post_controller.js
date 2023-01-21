0a38b27d5228f53da0b8056be71ead2a1d2848f5
Shashank Singh <ssingh0271@gmail.com>
2023-01-21 20:53:08 +0530

    finished updated post controller

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,4 +1,3 @@
-const { request, response } = require('express');
 const { StatusCodes } = require('http-status-codes');
 const { CustomAPIError } = require('../errors/custom-error');
 const { deleteImage } = require('../modules/cloudinaryApis/deleteFile');
@@ -6,14 +5,12 @@ const { uploadImage } = require('../modules/cloudinaryApis/createImage');
 const Blogpost = require('../models/Posts');
 
 
-const getAllPosts = async (req, res) => {
+const getAllPosts = async (_, res) => {
     result = await Blogpost.find();
     res.status(StatusCodes.OK).json({ "message": "fetched all posts", result });
 }
 
 const getMyPosts = async (req, res) => {
-    //queries are passed after : ? use req.query
-    //params are passed after '/' use req.params
     firebaseId = req.params.id;
     posts = await Blogpost.find({ userFirebaseId: firebaseId });
     res.status(StatusCodes.OK).json({ "message": "fetched my posts", posts });
@@ -22,14 +19,12 @@ const getMyPosts = async (req, res) => {
 const createPost = async (req, res) => {
     console.log("entered create post controller");
     const file = req.files.photo;
-    //upload post pic :
     const imageUrl = await uploadImage(file);
-    //creating post doc :
     req.body.imageUrl = imageUrl;
     console.log(req.body);
     try {
         post = await Blogpost.create(req.body);
-        res.status(StatusCodes.CREATED).json({ "message": "created my posts", post });
+        res.status(StatusCodes.CREATED).json(post);
     }
     catch (error) {
         await deleteImage(imageUrl);
@@ -43,11 +38,47 @@ const deletePost = async (req, res) => {
     if (!result) {
         throw new CustomAPIError('Post with id does not exist', StatusCodes.NOT_FOUND);
     }
-    deleteImage(result.imageUrl);
+    await deleteImage(result.imageUrl);
     res.status(StatusCodes.NO_CONTENT).json();
 }
 
 const updatePost = async (req, res) => {
+    let { title, body } = req.body;
+    const { id: postId } = req.params;
+    const file = req.file;
+    post = await Blogpost.findById(postId);
+    if (!post) {
+        throw new CustomAPIError('Post with id does not exist', StatusCodes.NOT_FOUND);
+    }
+    if (title != undefined) {//update title
+        post.title = title;
+    }
+    else {//if title is not provided by the user
+        title = post["title"];
+    }
+    if (body != undefined) {//update body
+        post.body = body;
+    }
+    else {//if body is not provided by the user
+        body = post["body"];
+    }
+    if (file != undefined) {//update blog image
+        const imageUrl = post.imageUrl;
+        await deleteImage(imageUrl);
+        const newImageUrl = await uploadImage(file);
+        post.imageUrl = newImageUrl;
+    }
+    else {
+        post.imageUrl = post["imageUrl"];
+    }
+    const updatedPost = await Blogpost.findByIdAndUpdate(
+        postId,
+        {
+            title: post.title,
+            body: post.body,
+            imageUrl: post.imageUrl,
+        }
+    );
     res.status(StatusCodes.OK).json({ "message": "updated post" });
 }
 
