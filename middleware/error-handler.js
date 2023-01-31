f0b67368d259b4ec6e02c837971dec32ded28fc6
Shashank0271 <ssingh0271@gmail.com>
2023-01-31 23:15:05 +0530

    added more tests for users

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,17 +1,22 @@
-const { StatusCodes } = require('http-status-codes');
-const { CustomAPIError } = require('../errors/custom-error')
+const { StatusCodes } = require("http-status-codes");
+const { CustomAPIError } = require("../errors/custom-error");
 const errorHandlerMiddleware = (err, req, res, next) => {
   console.log("entered error handler");
   if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
+    return res.status(err.statusCode).json({ msg: err.message });
   }
   if (err.name == "ValidationError") {
-    return res.status(StatusCodes.PRECONDITION_FAILED).json({ msg: err["_message"] });
+    return res
+      .status(StatusCodes.PRECONDITION_FAILED)
+      .json({ msg: err["_message"] });
   }
   //in case of unhandled error we get this :
   console.log(err.message);
-  return res.status(500).json({ msg: 'Something went wrong, please try again' })
-}
+  return res
+    .status(500)
+    .json({ msg: "Something went wrong, please try again" });
+};
 
-module.exports = errorHandlerMiddleware
-
+module.exports = {
+  errorHandlerMiddleware,
+};
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,32 +1,58 @@
-const request = require('supertest');
-const { MongoMemoryServer } = require('mongodb-memory-server');
-const startServer = require('../app');
-const User = require('../models/User');
-const { disconnectDB } = require('../db/connect');
+const request = require("supertest");
+const { MongoMemoryServer } = require("mongodb-memory-server");
+const startServer = require("../app");
+const { disconnectDB } = require("../db/connect");
+// import * as errorHandler from "../middleware/error-handler";
+const errorHandler = require("../middleware/error-handler");
 
 describe("user apis", () => {
-    let app;
-    beforeEach(async () => {
-        const mongoTestServer = await MongoMemoryServer.create();
-        app = startServer(mongoTestServer.getUri());
+  let app;
+  beforeAll(async () => {
+    const mongoTestServer = await MongoMemoryServer.create();
+    app = startServer(mongoTestServer.getUri());
+  });
+  afterAll(async () => {
+    await disconnectDB();
+  });
+  describe("USER POST/---- ", () => {
+    it("on giving details ,should responde with a 200 status code", async () => {
+      const response = await request(app).post("/api/v1/users").send({
+        username: "testusername",
+        email: "testemail@gmail.com",
+        firebaseUid: "firebasetestuid",
+        registrationToken: "fakeregtoken",
+      });
+      expect(response.statusCode).toBe(200);
+      expect(response.body).not.toBeNull;
     });
-    afterAll(async () => {
-        await disconnectDB().then(() => console.log("disconnected"));
-    });
-    describe("USER POST/---- ", () => {
-        //should save the user into the database 
-        //should response with a json object containing the user
-        it("on giving details ,should responde with a 200 status code", async () => {
-            await User.deleteOne({ firebaseUid: "firebasetestuid" });
-            const response = await request(app).post('/api/v1/users').send({
-                username: "testusername",
-                email: "testemail@gmail.com",
-                firebaseUid: "firebasetestuid",
-                registrationToken: "fakeregtoken",
-            });
-            await expect(response.statusCode).toBe(200);
-            expect(response.body).not.toBeNull;
+    it("should not accept incomplete details", async () => {
+      const { statusCode, body } = await request(app)
+        .post("/api/v1/users")
+        .send({
+          username: "testusername",
+          email: "testemailgail.com",
+          registrationToken: "fakeregtoken",
         });
+      expect(statusCode).not.toBe(200);
+      expect(body.msg).toBe("user validation failed");
     });
-
-})
+    it("should not insert duplicate emails", async () => {
+      await request(app).post("/api/v1/users").send({
+        username: "testusername",
+        email: "testemail@gmail.com",
+        firebaseUid: "firebasetestuid",
+        registrationToken: "fakeregtoken",
+      });
+      const { statusCode, body } = await request(app)
+        .post("/api/v1/users")
+        .send({
+          username: "testusername2",
+          email: "testemail@gmail.com",
+          firebaseUid: "firebasetestuid2",
+          registrationToken: "fakeregtoken2",
+        });
+      expect(statusCode).toBe(500);
+      expect(body.msg).toBe("Something went wrong, please try again");
+    });
+  });
+});
